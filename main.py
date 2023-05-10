209d31c8ddec259ddef4840d5fa5306839e6e84e
Geovanna Melo <gsfmelo@gmail.com>
2023-05-10 12:01:51 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,12 @@
+kwh = float(input('Quantos kwh?')
+tipo = input('Qual o tipo de instalação (R, C ou I)')
+
+if (tipo == 'R'):
+elif (tipo == 'C'):
+
+elif (tipo == 'I'):
+
+else:
+print('Instalação inválida')
+
+print('olá mundo')
