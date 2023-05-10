8da16f0eb4da99ec199c5bc69a0494e24d43fb2c
Geovanna Melo <gsfmelo@gmail.com>
2023-05-10 12:11:53 -0300

    Alterando o main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-kwh = float(input('Quantos kwh?')
-tipo = input('Qual o tipo de instalação (R, C ou I)')
-
-if (tipo == 'R'):
-elif (tipo == 'C'):
-
-elif (tipo == 'I'):
-
-else:
-print('Instalação inválida')
-
-print('olá mundo')
+print('amo o mundo')
